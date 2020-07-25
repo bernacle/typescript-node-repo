df03506297d9c80779db4a1c0b636ef685c3c618
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-07-25 17:59:34 -0300

    appointments rules

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import ensureAuthenticated from "@modules/users/infra/http/middleware/ensureAuthenticated";
 import ProvidersController from "../controllers/ProvidersController";
+import ProvidersDayAvailabilityController from "../controllers/ProvidersDayAvailabilityController";
+import ProvidersMonthAvailabilityController from "../controllers/ProvidersMonthAvailabilityController";
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providersDayAvailabilityController = new ProvidersDayAvailabilityController();
+const providersMonthAvailabilityController = new ProvidersMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get("/", providersController.index);
+
+providersRouter.get(
+  "/:provider_id/day-availability",
+  providersDayAvailabilityController.index,
+);
+providersRouter.get(
+  "/:provider_id/month-availability",
+  providersMonthAvailabilityController.index,
+);
 
 export default providersRouter;
