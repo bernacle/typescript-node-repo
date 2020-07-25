2632af10813f1a55ddf3dd16efbe8061bf07217f
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-07-25 10:09:44 -0300

    appointmens availability day

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,63 @@
+import { injectable, inject } from "tsyringe";
+import User from "@modules/users/infra/typeorm/entities/User";
+import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
+import { getHours, getDate } from "date-fns";
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject("AppointmentsRepository")
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      },
+    );
+
+    //cria um array de tamanho especifico e iniciando de uma posicao definida
+    // começando de 8 + 10 [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    const hourStart = 8;
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
