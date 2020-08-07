f1b854450f18c4a9b274bce983fb5953ce4224ed
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-08-06 21:49:47 -0300

    configuring s3 upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,19 +1,48 @@
 import path from "path";
 import crypto from "crypto";
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
 
-const tpmFolder = path.resolve(__dirname, "..", "..", "tmp");
+const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+
+interface IUploadConfig {
+  driver: "s3" | "disk";
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
-  tpmFolder,
-  uploadsFolder: path.resolve(tpmFolder, "uploads"),
-  storage: multer.diskStorage({
-    destination: tpmFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString("HEX");
-      const filename = `${fileHash}-${file.originalname}`;
+  driver: process.env.STORAGE_DRIVER,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, "uploads"),
 
-      return callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString("HEX");
+        const filename = `${fileHash}-${file.originalname}`;
+
+        return callback(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: "app-node-first",
     },
-  }),
-};
+  },
+} as IUploadConfig;
