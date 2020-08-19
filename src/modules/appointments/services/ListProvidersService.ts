5e80f309202a99ee6fdcfcb9dde87c28094e88f3
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-08-19 12:13:29 -0300

    gobarber mobile

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from "tsyringe";
 import User from "@modules/users/infra/typeorm/entities/User";
 import IUserRepository from "@modules/users/repositories/IUsersRepository";
 import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
+import { classToClass } from "class-transformer";
 
 interface IRequest {
   user_id: string;
@@ -13,7 +14,7 @@ class ListProvidersService {
     @inject("UsersRepository") private usersRepository: IUserRepository,
     @inject("CacheProvider")
     private cacheProvider: ICacheProvider,
-  ) {}
+  ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
     let users = await this.cacheProvider.recover<User[]>(
@@ -25,7 +26,10 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
